09530a82bbdd1f6888da5916d472d2cd073c2988
steven23sp <steven23s@outlook.es>
2020-10-09 15:53:58 -0500

    version 3

--- apps/views.py
+++ apps/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render, render_to_response
 # Create your views here.
 def menu(request):
     data = {
-        'titulo': 'Menu Principal', 'entidad': 'Menu Principal'
+        'title': 'Menu Principal', 'entidad': 'Menu Principal'
     }
     return render(request, 'prueba/index.html', data)
